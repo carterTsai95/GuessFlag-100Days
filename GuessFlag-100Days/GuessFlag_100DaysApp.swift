e6acf8fdfa4b4e5fbdfed0dafcf767c60372f234
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-01-08 15:31:11 -0500

    Initial Commit

---
+++ GuessFlag-100Days/GuessFlag_100DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlag_100DaysApp.swift
+//  GuessFlag-100Days
+//
+//  Created by Hung-Chun Tsai on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlag_100DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
