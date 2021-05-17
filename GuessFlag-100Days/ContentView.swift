d1d8a73d570e1f4ca3b366d6d20842ff8d40a1ab
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-05-17 12:38:11 -0400

    Add app screenshot

--- GuessFlag-100Days/ContentView.swift
+++ GuessFlag-100Days/ContentView.swift
@@ -86,8 +86,6 @@ struct ContentView: View {
 /*
 
  1. -(DONE)- Add an @State property to store the user’s score, modify it when they get an answer right or wrong, then display it in the alert.
- 2. Show the player’s current score in a label directly below the flags.
- When someone chooses the wrong flag, tell them their mistake in your alert message – something like “Wrong! That’s the flag of France,” for example.
  */
 
 struct ContentView_Previews: PreviewProvider {
